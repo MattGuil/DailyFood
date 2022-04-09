3c619f1f609809c77a6cb3a239bc4a35dd56e74e
MattGuil <guillemin.matt@gmail.com>
2022-04-09 16:10:17 +0200

    dynamic average price

--- MiniProject.Data/Models/Restaurant.cs
+++ MiniProject.Data/Models/Restaurant.cs
@@ -13,22 +13,32 @@ namespace MiniProject.Data.Models
         public string typeOfFood { get; set; }
         public Menu menu { get; set; }
         public string address { get; set; }
-        public int averagePrice { get; set; }
         public int reduction { get; set; }
         public float stars { get; set; }
         public string image { get; set; }
 
-        public Restaurant(int id, string name, string typeOfFood, Menu menu, string address, int averagePrice, int reduction, float stars, string image)
+        public Restaurant(int id, string name, string typeOfFood, Menu menu, string address, int reduction, float stars, string image)
         {
             this.id = id;
             this.name = name;
             this.typeOfFood = typeOfFood;
             this.menu = menu;
             this.address = address;
-            this.averagePrice = averagePrice;
             this.reduction = reduction;
             this.stars = stars;
             this.image = image;
+        }
+
+        public float getAveragePrice()
+        {
+            int count = 0;
+            float sumPrices = 0;
+            foreach (Dish dish in menu.getDishes())
+            {
+                sumPrices += dish.Price;
+                count++;
+            }
+            return sumPrices / count;
         }
     }
 }
--- MiniProject.Data/Services/RestaurantDataProvider.cs
+++ MiniProject.Data/Services/RestaurantDataProvider.cs
@@ -27,13 +27,13 @@ namespace MiniProject.Data.Services
                 if (instance == null)
                 {
                     instance = new RestaurantDataProvider();
-                    instance.restaurants.Add(new Restaurant(0, "", "", instance.menuDataProvider.GetMenu(""), "", 0, 0, (float)0.0, ""));
-                    instance.restaurants.Add(new Restaurant(1, "La Monnalisa", "Italian", instance.menuDataProvider.GetMenu("La Monnalisa"), "8 Patriot Square E2 9NF", 35, 30, (float)9.5, "la-monnalisa.jpg"));
-                    instance.restaurants.Add(new Restaurant(2, "Alliance", "Mexican", instance.menuDataProvider.GetMenu("Alliance"), "27 Old Gloucester St, 4563", 30, 40, (float)8.0, "alliance.jpg"));
-                    instance.restaurants.Add(new Restaurant(3, "Sushi Gold", "Sushi - Japanese", instance.menuDataProvider.GetMenu("Sushi Gold"), "Old Shire Ln EN9 3RX", 20, 25, (float)9.0, "sushi-gold.jpg"));
-                    instance.restaurants.Add(new Restaurant(4, "Mr. Pepper", "Vegetarian", instance.menuDataProvider.GetMenu("Mr. Pepper"), "27 Old Gloucester St, 4563", 20, 30, (float)9.5, "mr-pepper.jpg"));
-                    instance.restaurants.Add(new Restaurant(5, "Dragon Tower", "Chinese", instance.menuDataProvider.GetMenu("Dragon Tower"), "22 Hertsmere Rd E14 4ED", 35, 50, (float)8.0, "dragon-tower.jpg"));
-                    instance.restaurants.Add(new Restaurant(6, "Bella Napoli", "Pizza - Italian", instance.menuDataProvider.GetMenu("Bella Napoli"), "135 Newtownards Road BT4", 25, 45, (float)8.5, "bella-napoli.jpg"));
+                    instance.restaurants.Add(new Restaurant(0, "", "", instance.menuDataProvider.GetMenu(""), "", 0, (float)0.0, ""));
+                    instance.restaurants.Add(new Restaurant(1, "La Monnalisa", "Italian", instance.menuDataProvider.GetMenu("La Monnalisa"), "8 Patriot Square E2 9NF", 30, (float)9.5, "la-monnalisa.jpg"));
+                    instance.restaurants.Add(new Restaurant(2, "Alliance", "Mexican", instance.menuDataProvider.GetMenu("Alliance"), "27 Old Gloucester St, 4563", 40, (float)8.0, "alliance.jpg"));
+                    instance.restaurants.Add(new Restaurant(3, "Sushi Gold", "Sushi - Japanese", instance.menuDataProvider.GetMenu("Sushi Gold"), "Old Shire Ln EN9 3RX", 25, (float)9.0, "sushi-gold.jpg"));
+                    instance.restaurants.Add(new Restaurant(4, "Mr. Pepper", "Vegetarian", instance.menuDataProvider.GetMenu("Mr. Pepper"), "27 Old Gloucester St, 4563", 30, (float)9.5, "mr-pepper.jpg"));
+                    instance.restaurants.Add(new Restaurant(5, "Dragon Tower", "Chinese", instance.menuDataProvider.GetMenu("Dragon Tower"), "22 Hertsmere Rd E14 4ED", 50, (float)8.0, "dragon-tower.jpg"));
+                    instance.restaurants.Add(new Restaurant(6, "Bella Napoli", "Pizza - Italian", instance.menuDataProvider.GetMenu("Bella Napoli"), "135 Newtownards Road BT4", 45, (float)8.5, "bella-napoli.jpg"));
                 }
                 return instance;
             }
